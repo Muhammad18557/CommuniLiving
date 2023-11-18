bba4a6413ad4c79c1cc2ccaa0cefde698e90baf0
Muhammad18557 <muhammad.abdullah@u.yale-nus.edu.sg>
2023-11-17 22:32:16 -0500

    release for alpha version

--- frontend/app/components/BookingCard/BookingCard.tsx
+++ frontend/app/components/BookingCard/BookingCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import PropTypes from 'prop-types';
 import './BookingCard.css';
+import Link from 'next/link';
 
 interface BookingCardProps {
   name: string;
@@ -42,7 +43,9 @@ name,
             <br />
         </span>
     </p>
-    <div className="text-wrapper-3">Open community dashboard</div>
+    <div className="text-wrapper-3"> 
+    <Link href='/createbooking'> Open community booking page </Link>
+    </div>
     </div>
     
 
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import BookingCard from '../components/BookingCard/BookingCard';
 import "./userDashboard.css";
 import bookingData from './bookingData.json';
+import Link from 'next/link'; 
 
 export const UserDashboard = () => {
   return (
@@ -9,6 +10,9 @@ export const UserDashboard = () => {
       <div className="div">
         <div className="page-intro">
           <div className="page-title"><h1>User Dashboard</h1></div>
+          <div> Sample Booking Form (To be changed to calender later)
+            <Link href='/createbooking' style={{ color: 'green' }}> Booking Page for a Community</Link>
+          </div>
           <div className="page-info"><h5>Here are your communities</h5></div>
         </div>
       </div>
