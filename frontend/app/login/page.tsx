dc540fbfe35f44e274ff7b532560d546a22a7b2b
martinjedreilim <martin.lim511@gmail.com>
2023-12-02 15:28:58 -0500

    -

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -5,8 +5,6 @@ import './login.css';
 
 
 
-
-
 import Auth from "../components/Body/authentication/Auth";
 
 function Login() {
@@ -53,10 +51,6 @@ function Login() {
           <div className="d-grid gap-2 mt-3">
             <button type="submit" className="btn btn-primary">Submit</button>
           </div>
-          <br></br>
-          <div>
-        <p>If you don't have an account yet, <a href="#" id="signUpLink">click here to sign up</a>.</p>
-    </div>
         </div>
       </form>
 
