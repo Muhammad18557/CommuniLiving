e6e64e957a05bd660f019913c2f2719072f1c074
Muhammad18557 <muhammad.abdullah@u.yale-nus.edu.sg>
2023-12-03 19:53:56 -0500

    first

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -13,54 +13,53 @@ function Login() {
 
   return (
 
-    <div className="Auth-form-container">
-      <br></br>
-      <br></br>
-      <img
-            className="logo-image"
-            src="https://communitylivinginc.org/wp-content/uploads/2016/05/CLI-Logo-cropped.png"
-            alt="Your Company"
-          />
-      <br></br>
-      <br></br>
-      <form className="Auth-form" onSubmit={submit}>
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
-          <div className="form-group mt-3">
-            <label>Username</label>
-            <input
-              className="form-control mt-1"
-              placeholder="Enter Username"
-              name='username'
-              type='text'
-              value={username}
-              required
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              name='password'
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-              value={password}
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </div>
-        </div>
-      </form>
+    // <div className="Auth-form-container">
+    //   <br></br>
+    //   <br></br>
+    //   <img
+    //         className="logo-image"
+    //         src="https://communitylivinginc.org/wp-content/uploads/2016/05/CLI-Logo-cropped.png"
+    //         alt="Your Company"
+    //       />
+    //   <br></br>
+    //   <br></br>
+    //   <form className="Auth-form" onSubmit={submit}>
+    //     <div className="Auth-form-content">
+    //       <h3 className="Auth-form-title">Sign In</h3>
+    //       <div className="form-group mt-3">
+    //         <label>Username</label>
+    //         <input
+    //           className="form-control mt-1"
+    //           placeholder="Enter Username"
+    //           name='username'
+    //           type='text'
+    //           value={username}
+    //           required
+    //           onChange={(e) => setUsername(e.target.value)}
+    //         />
+    //       </div>
+    //       <div className="form-group mt-3">
+    //         <label>Password</label>
+    //         <input
+    //           name='password'
+    //           type="password"
+    //           className="form-control mt-1"
+    //           placeholder="Enter password"
+    //           value={password}
+    //           required
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //       </div>
+    //       <div className="d-grid gap-2 mt-3">
+    //         <button type="submit" className="btn btn-primary">Submit</button>
+    //       </div>
+    //     </div>
+    //   </form>
 
-    <div>
-      <Auth isSignUp={false} />
+    // <div>
+    //   <Auth isSignUp={false} />
 
-    </div>
+    // </div>
   );
-}
 
 export default Login;
